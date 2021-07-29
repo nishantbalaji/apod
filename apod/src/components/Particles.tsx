adc7f436841722a17c815ae247515019f8b1e74b
Nishant Balaji <nishantb1130@gmail.com>
2021-07-29 11:55:24 -0700

    nasa api connection

---
+++ apod/src/components/Particles.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+// import Particles from 'react-particles-js';
+// import particlesProps from 'particlesjs-config.json';
+
+const ParticlesBG = () => {
+    return (
+        // <Particles/>
+        <>
+        </>
+    )
+}
+
+export default ParticlesBG;
